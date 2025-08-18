f7b62ee35ec327ea3c0326955eab1db95453c6a8
sharpwall39 <30500096+sharpwall39@users.noreply.github.com>
2025-08-18 17:10:29 -0400

    Create jscollapse.js

---
+++ jscollapse.js
@@ -0,0 +1,8 @@
+function toggleAnalysis(id) {
+  const content = document.getElementById(id);
+  if (content.style.display === "none") {
+    content.style.display = "block";
+  } else {
+    content.style.display = "none";
+  }
+}

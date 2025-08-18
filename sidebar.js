0713e35018a90172fd05942d802322013393c900
sharpwall39 <30500096+sharpwall39@users.noreply.github.com>
2025-08-18 10:35:37 -0400

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,11 +1,20 @@
 function toggleSidebar() {
   const sidebar = document.querySelector(".sidebar");
+  const overlay = document.getElementById("overlay");
+
   sidebar.classList.toggle("open");
+  overlay.classList.toggle("active");
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   const menuBtn = document.getElementById("menu-btn");
+  const overlay = document.getElementById("overlay");
+
   if (menuBtn) {
     menuBtn.addEventListener("click", toggleSidebar);
+  }
+
+  if (overlay) {
+    overlay.addEventListener("click", toggleSidebar); // click outside closes
   }
 });
